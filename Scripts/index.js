7786df624512adcc23e1843470f665837fa2cd44
Victor Kipkurui <kipkurui.victor18@students.dkut.ac.ke>
2024-09-13 14:02:10 +0300

    first changes

---
+++ Scripts/index.js
@@ -0,0 +1,24 @@
+function openServiceDetails(service) {
+   let url = '';
+    switch (service) {
+        case 'all':
+            url = 'all.html';
+            break;
+        case 'labor-delivery':
+            url = 'labor-delivery.html';
+            break;
+        case 'pain-management':
+            url = 'pain-management.html';
+            break;
+        case 'postnatal-care':
+            url = 'postnatal-care.html';
+            break;
+        case 'nicu':
+            url = 'nicu.html';
+            break;
+        default:
+            url = 'index.html'; 
+    }
+
+        window.location.href = url;
+    }
