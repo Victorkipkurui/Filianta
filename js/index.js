5823bbf2c3a8888636c8fedcc37ed55da2cf1a25
Victor Kipkurui <kipkurui.victor18@students.dkut.ac.ke>
2024-09-14 13:26:41 +0300

    javascript

--- js/index.js
+++ js/index.js
@@ -29,4 +29,66 @@ const navbar = document.querySelector('.navbar');
 menuIcon.addEventListener('click', () => {
 navbar.classList.toggle('active');
 });
+
+function countUp(element) {
+    const target = +element.getAttribute('data-target');
+    const increment = target / 100;
+    let count = 0;
+  
+    const updateCount = () => {
+      count += increment;
+      
+      if (count < target) {
+        element.textContent = Math.ceil(count);
+        requestAnimationFrame(updateCount);
+      } else {
+        
+        if (format === 'percent') {
+          element.textContent = `${target}%`;
+        } else if (format === 'plus') {
+          element.textContent = `${target}+`;
+        } else {
+          element.textContent = target;
+        }
+      }
+    };
+  
+    updateCount();
+  }
+  
+const counters = document.querySelectorAll('.stat');
+
+counters.forEach(counter => {
+if (counter.id === 'count') {
+    countUp(counter, 'percent'); 
+    } else if (counter.id === 'users') {
+    countUp(counter, 'plus'); 
+    } else {
+    countUp(counter); 
+    }
+});
+
+const modal = document.getElementById("videoModal");
+const btn = document.getElementById("playButton");
+const span = document.getElementsByClassName("close")[0];
+const video = document.getElementById("videoPlayer");
+
+btn.onclick = function() {
+modal.style.display = "flex";
+video.play();
+}
+
+// When the user clicks on (x), close the modal and pause the video
+span.onclick = function() {
+modal.style.display = "none";
+video.pause();
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+if (event.target == modal) {
+    modal.style.display = "none";
+    video.pause();
+}
+}
     
